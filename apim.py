96174e8bf2b170b5c83689a5029647ff86db92b6
esrakalkan <esrakalkan3000@gmail.com>
2018-08-02 16:22:57 +0300

    okunabilirlik artırıldı

--- apim.py
+++ apim.py
@@ -48,18 +48,11 @@ while True:
     sehir=buyut(sehir)
 
     weather_main=hava.get_info(sehir).get("weather")[0].get("main")
-
     description = hava.get_info(sehir).get("weather")[0].get("description")
-
     temp = hava.get_info(sehir).get("main").get("temp")
-
     humidity=hava.get_info(sehir).get("main").get("humidity")
-
-
     temp_min = hava.get_info(sehir).get("main").get("temp_min")
-
     temp_max = hava.get_info(sehir).get("main").get("temp_max")
-
     speed = hava.get_info(sehir).get("wind").get("speed")
 
     my_wheather_list = [weather_main, description, temp, humidity, temp_min, temp_max, speed]
