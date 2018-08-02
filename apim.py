b28bbf3a424e034dcb4b45682b06083bd3c66aed
yldrmzffr <muzaffer@yldrmzffr.com>
2018-08-02 12:06:44 +0300

    api bağlatı başarılı

---
+++ apim.py
@@ -0,0 +1,15 @@
+import json
+import requests
+class havalar():
+
+    sehir = None
+
+    def __init__(self):
+        pass
+    def get_info(self,sehir):
+        self.sehir = sehir
+        data = requests.get("http://api.openweathermap.org/data/2.5/weather?q={id}&appid=fe76c224322cdde0049342cb45d26b04".format(id=sehir)).content.decode('utf-8')
+        return data
+
+hava = havalar()
+hava.get_info("KONYA")
