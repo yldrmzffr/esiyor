6f79e68cc3a8053bce296921840af9747e490485
elifacikgoz <41567034+elifacikgoz@users.noreply.github.com>
2018-08-02 12:31:30 +0300

    Update apim.py

--- apim.py
+++ apim.py
@@ -8,8 +8,11 @@ class havalar():
         pass
     def get_info(self,sehir):
         self.sehir = sehir
-        data = requests.get("http://api.openweathermap.org/data/2.5/weather?q={id}&appid=fe76c224322cdde0049342cb45d26b04".format(id=sehir)).content.decode('utf-8')
-        return data
+        try:
+            data = requests.get("http://api.openweathermap.org/data/2.5/weather?q={id}&appid=fe76c224322cdde0049342cb45d26b04".format(id=sehir)).content.decode('utf-8')
+            return data
+         except:
+            print("Error: Can not reach ")
 
 hava = havalar()
 hava.get_info("KONYA")
