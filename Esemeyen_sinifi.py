f3759385bd2dcd9fe98fac175e595055b2fac77b
Laviniaa <elif.acikgoz@gmail.com>
2018-08-02 14:02:17 +0300

    eksik dosya elendi

---
+++ Esemeyen_sinifi.py
@@ -0,0 +1,19 @@
+import json
+import requests
+class Havalar():
+    sehir = None
+    def init(self):
+        pass
+    def get_info(self,sehir):
+        self.sehir = sehir
+        try:
+            data = requests.get("http://api.openweathermap.org/data/2.5/weather?q={id}&appid=fe76c224322cdde0049342cb45d26b04".format(id=sehir)).content.decode('utf-8')
+            return data
+        except:
+            print("Error: Can not reach ")
+
+
+    def auto_get(self):
+        data = json.loads(requests.get("https://ipapi.co/json").content.decode('utf-8')).get("city")
+        self.sehir = data
+        return data
