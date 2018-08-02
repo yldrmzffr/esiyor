e09ffaaabb92f7b087a910a6cd2b6601d8708174
esrakalkan <esrakalkan3000@gmail.com>
2018-08-02 13:16:51 +0300

    menu ve sehir seçme

--- menu.py
+++ menu.py
@@ -1,24 +1,3 @@
-import json
-import requests
-class havalar():
-    sehir = None
-    def init(self):
-        pass
-    def get_info(self,sehir):
-        self.sehir = sehir
-        try:
-            data = requests.get("http://api.openweathermap.org/data/2.5/weather?q={id}&appid=fe76c224322cdde0049342cb45d26b04".format(id=sehir)).content.decode('utf-8')
-            return data
-         except:
-            print("Error: Can not reach ")
-
-
-    def auto_get(self):
-        data = json.loads(requests.get("https://ipapi.co/json").content.decode('utf-8')).get("city")
-        self.sehir = data
-        return data
-
-hava = havalar()
 
 def sehir_sec(sehir):
     print("suan {} şehrindesiniz".format(sehir))
@@ -48,5 +27,5 @@ while True:
     print("*******************\n")
     print("    HOSGELDİNİZ      \n")
     print("*******************\n")
-    sehir_sec(hava.auto_get())
+    sehir_sec("sehir")
 
