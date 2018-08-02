0dd38f15b2e029f1cd7fcd2c04eb24f282b02bbb
Laviniaa <elif.acikgoz@gmail.com>
2018-08-02 16:52:46 +0300

    kelvin to cel

--- apim.py
+++ apim.py
@@ -36,8 +36,7 @@ def menu():
         menu()
 
 print("*******************\n")
-print("    WELCOME      \n")
-print("\n")
+print("      WELCOME    \n ")
 print("*******************\n")
 
 
@@ -50,9 +49,12 @@ while True:
     weather_main=hava.get_info(sehir).get("weather")[0].get("main")
     description = hava.get_info(sehir).get("weather")[0].get("description")
     temp = hava.get_info(sehir).get("main").get("temp")
+    temp=int(temp)-272.
     humidity=hava.get_info(sehir).get("main").get("humidity")
     temp_min = hava.get_info(sehir).get("main").get("temp_min")
+    temp_min=int(temp_min)-272
     temp_max = hava.get_info(sehir).get("main").get("temp_max")
+    temp_max=int(temp_max)-272
     speed = hava.get_info(sehir).get("wind").get("speed")
 
     my_wheather_list = [weather_main, description, temp, humidity, temp_min, temp_max, speed]
