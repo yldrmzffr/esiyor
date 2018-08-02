92bee413f7b7d240ee075b646b66328b0e2e041a
Laviniaa <elif.acikgoz@gmail.com>
2018-08-02 13:44:18 +0300

    Menu duzenlendi

--- apim.py
+++ apim.py
@@ -20,33 +20,32 @@ class havalar():
 
 hava = havalar()
 
-def sehir_sec(sehir):
-    print("suan {} şehrindesiniz".format(sehir))
+def menu():
+    konum=hava.auto_get()
+    print("suan {} şehrindesiniz".format(konum))
     print("yapmak istediğiniz işlemi seçiniz : ")
     print("0 : Bulunduğunuz şehrin hava durumu")
     print("1 : Baska bir şehir")
     secim = input("seciminizi yapınız : ")
 
-    if secim.isnumeric():
-        secim = int(secim)
-    else:
-        sehir_sec(sehir)
 
-    if secim == 0:
-       return sehir
+    if secim == "0":
+       ## Konuma gore cagir.
+       return konum
 
-    elif secim == 1:
+
+    elif secim == "1":
         sehir=input("şehir ismi giriniz : ")
         sehir=sehir.upper()
         return sehir
 
     else:
         print("Yanlış seçim yaptınız.Lütfen tekrar seçim yapınız : \n")
-        sehir_sec(sehir)
+        menu(konum)
 
 while True:
     print("*******************\n")
     print("    HOSGELDİNİZ      \n")
     print("\n")
     print("*******************\n")
-    sehir_sec(hava.auto_get())
+    sehir=menu()
